b9309a2ca4163dc44dbe4d7a11df8d473625097f
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-23 15:05:24 +0100

    Add card component and insert main content

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,39 +1,165 @@
 <script>
 import MainShopSection from './MainShopSection.vue'
+import ProductCard from './ProductCard.vue';
 
 export default {
-    components: { MainShopSection }
+    components: { MainShopSection, ProductCard },
+    data() {
+        return {
+            currentSeries: [
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX',
+                    price: '$19.99',
+                    series: 'Action Comics',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01',
+                    price: '$3.99',
+                    series: 'American Vampire 1976',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb',
+                    price: '$16.99',
+                    series: 'Aquaman',
+                    type: 'graphic novel',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76',
+                    price: '$2.99',
+                    series: 'Batgirl',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
+                    price: '$3.99',
+                    series: 'Batman',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
+                    price: '$2.99',
+                    series: 'Batman Beyond',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8',
+                    price: '$3.99',
+                    series: 'Batman/Superman',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk',
+                    price: '$4.99',
+                    series: 'Batman/Superman Annual',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8',
+                    price: '$5.99',
+                    series: 'Batman: The Joker War Zone',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY',
+                    price: '$6.99',
+                    series: 'Batman: Three Jokers',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX',
+                    price: '$4.99',
+                    series: 'Batman: White Knight Presents: Harley Quinn',
+                    type: 'comic book',
+                },
+                {
+                    thumb:
+                        'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq',
+                    price: '$16.99',
+                    series: 'Catwoman',
+                    type: 'graphic novel',
+                },
+            ]
+
+        }
+    }
 }
+
 </script>
 
 <template>
     <main>
         <section id="main-top">
-            <div class="container">
-                <h1> CONTENT GOES HERE </h1>
+            <div class="series-title">
+                <h1> CURRENT SERIES </h1>
+
+            </div>
+            <div class="container series-container">
+                <product-card v-for="currentSerie in currentSeries" :key="currentSerie.series"
+                    :currentSeries="currentSerie"></product-card>
+            </div>
+            <div class="load-more-button">
+                <button>LOAD MORE</button>
             </div>
         </section>
         <main-shop-section></main-shop-section>
     </main>
 </template>
 
-<style lang="scss" scope>
+<style lang="scss" scoped>
 //IMPORT MY COLOR PALETTE
 @use '../assets/scss/portials/variables' as *;
 @use '../assets/scss/portials/mixins' as *;
+
+button {
+    padding: 10px 50px;
+    background-color: $blue;
+    font-weight: bold;
+    color: white;
+    border: none;
+}
 
 main {
 
     #main-top {
         background-color: $grey;
-        height: 150px;
+        min-height: 150px;
+        position: relative;
 
-        h1 {
-            height: 100%;
+        .series-title {
+            position: absolute;
+            bottom: 95%;
+            left: 15%;
             color: white;
-            @include flex_center;
+            padding: 10px 30px;
+            background-color: $blue;
+            display: inline-block;
         }
 
+        .series-container {
+            padding: 30px 0;
+            display: flex;
+            flex-wrap: wrap;
+
+        }
+
+        .load-more-button {
+            margin: 0 auto;
+            text-align: center;
+            padding: 20px;
+        }
     }
 }
 
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+    name: 'Product Card',
+    props: {
+        currentSeries: Array,
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="series-col">
+        <figure>
+            <img :src="currentSeries.thumb" alt="">
+        </figure>
+        <p>{{ currentSeries.series }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.series-col {
+    text-transform: uppercase;
+    color: white;
+    padding: 1rem;
+    flex-basis: calc(100% / 6);
+
+    figure {
+        height: 180px;
+        overflow: hidden;
+
+        img {
+            height: auto;
+            width: 180px;
+        }
+    }
+
+    p {
+        padding: 10px 0;
+    }
+}
+</style>
